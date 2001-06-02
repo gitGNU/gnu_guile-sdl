0db8d5268ff8646fa9983214ad414aa51000bf86
Alex Shinn <foof@synthcode.com>
2001-06-02 02:10:48 +0000

    Adding init/quit subsystems and initialization constants.

--- guile-sdl.c
+++ guile-sdl.c
@@ -2,7 +2,7 @@
  *  guile-sdl.c -- SDL Video Wrappers for Guile                    *
  *                                                                 *
  *  Created:    <2001-04-08 13:48:18 foof>                         *
- *  Time-stamp: <2001-05-16 00:24:39 foof>                         *
+ *  Time-stamp: <2001-06-01 21:38:41 foof>                         *
  *  Author:     Alex Shinn <foof@debian.org>                       *
  *                                                                 *
  *  Copyright (C) 2001 Alex Shinn                                  *
@@ -29,7 +29,6 @@
 #include <SDL/SDL.h>
 /* wrapper headers */
 #include "video.h"
-#include "image.h"
 
 /* Initialization */
 static SCM
@@ -37,10 +36,21 @@ sdl_init (SCM s_subsystems)
 {
    int subsystems;
 
-   SCM_ASSERT(SCM_INUMP(s_subsystems), s_subsystems, SCM_ARG1, "sdl-init");
-   subsystems = SCM_INUM(s_subsystems);
+   SCM_ASSERT (SCM_INUMP (s_subsystems), s_subsystems, SCM_ARG1, "init");
+   subsystems = SCM_INUM (s_subsystems);
 
-   return SCM_MAKINUM(SDL_Init(subsystems));
+   return SCM_MAKINUM (SDL_Init (subsystems));
+}
+
+static SCM
+sdl_init_subsystem (SCM s_subsystems)
+{
+   int subsystems;
+
+   SCM_ASSERT (SCM_INUMP (s_subsystems), s_subsystems, SCM_ARG1, "init-subsystem");
+   subsystems = SCM_INUM (s_subsystems);
+
+   return SCM_MAKINUM (SDL_InitSubSystem (subsystems));
 }
 
 /* Termination */
@@ -51,32 +61,62 @@ sdl_quit (void)
    return SCM_UNSPECIFIED;
 }
 
+static SCM
+sdl_quit_subsystem (SCM s_subsystems)
+{
+   int subsystems;
+
+   SCM_ASSERT (SCM_INUMP (s_subsystems), s_subsystems, SCM_ARG1, "quit-subsystem");
+   subsystems = SCM_INUM (s_subsystems);
+
+   SDL_QuitSubSystem (subsystems);
+   return SCM_UNSPECIFIED;
+}
+
+/* Information */
+static SCM
+sdl_was_init (SCM s_subsystems)
+{
+   int subsystems;
+
+   SCM_ASSERT (SCM_INUMP (s_subsystems), s_subsystems, SCM_ARG1, "was-init");
+   subsystems = SCM_INUM (s_subsystems);
+
+   return SCM_MAKINUM (SDL_WasInit (subsystems));
+}
+
 void
 guile_sdl_init (void)
 {
    /* general initializations */
-   scm_make_gsubr ("sdl-init", 1, 0, 0, sdl_init);
-   scm_make_gsubr ("sdl-quit", 0, 0, 0, sdl_quit);
+   scm_make_gsubr ("init",           1, 0, 0, sdl_init);
+   scm_make_gsubr ("init-subsystem", 1, 0, 0, sdl_init_subsystem);
+   scm_make_gsubr ("quit-sdl",       0, 0, 0, sdl_quit);
+   scm_make_gsubr ("quit-subsystem", 1, 0, 0, sdl_quit_subsystem);
+   scm_make_gsubr ("was-init",       1, 0, 0, sdl_was_init);
+
+   /* constants */
+   scm_c_define ("init-timer",       SCM_MAKINUM (SDL_INIT_TIMER));
+   scm_c_define ("init-audio",       SCM_MAKINUM (SDL_INIT_AUDIO));
+   scm_c_define ("init-video",       SCM_MAKINUM (SDL_INIT_VIDEO));
+   scm_c_define ("init-cdrom",       SCM_MAKINUM (SDL_INIT_CDROM));
+   scm_c_define ("init-joystick",    SCM_MAKINUM (SDL_INIT_JOYSTICK));
+   scm_c_define ("init-everything",  SCM_MAKINUM (SDL_INIT_EVERYTHING));
+   scm_c_define ("init-noparachute", SCM_MAKINUM (SDL_INIT_NOPARACHUTE));
+   scm_c_define ("init-eventthread", SCM_MAKINUM (SDL_INIT_EVENTTHREAD));
+
+   /* exported symbols */
+   scm_c_export ("init", "quit-sdl", "init-subsystem",
+                 "quit-subsystem", "was-init",
+                 /* constants */
+                 "init-timer", "init-audio", "init-video",
+                 "init-cdrom", "init-joystick", "init-everything",
+                 "init-noparachute", "init-eventthread",
+                 NULL);
 
    /* video initializations */
    sdl_video_init();
 
-   /* image initializations */
-/*    sdl_image_init(); */
+   /* input initializations */
 }
-
-/* static void */
-/* inner_main (void *closure, int argc, char **argv) */
-/* { */
-/*    /\* module initializations would go here *\/ */
-/*    guile_sdl_init(); */
-/*    scm_shell (argc, argv); */
-/* } */
-
-/* int */
-/* main (int argc, char **argv) */
-/* { */
-/*    scm_boot_guile (argc, argv, inner_main, 0); */
-/*    return 0; /\* never reached *\/ */
-/* } */
 
