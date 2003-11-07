fef332abd7718c5f017140e640ff2efb25d43fde
Thien-Thi Nguyen <ttn@gnuvola.org>
2003-11-07 15:41:32 +0000

    No longer include <libguile.h>. (ASSERT_SMOB): Rename from SCM_ASSERT_SMOB. No longer take 4th arg; instead, hardcode `FUNC_NAME'. (GSDL_NUMBER_GETTER): Rename from SCM_DEFINE_NUMBER_GETTER, rewrite to use MDEFLOCEXP, and move into new file wholefns.h. (GSDL_NUMBER_SETTER): Likewise, from SCM_DEFINE_NUMBER_SETTER. (GSDL_ENUM_GETTER): Likewise, from SCM_DEFINE_ENUM_GETTER. (GSDL_ENUM_SETTER): Likewise, from SCM_DEFINE_ENUM_SETTER. (GSDL_FLAG_GETTER): Likewise, from SCM_DEFINE_FLAG_GETTER. (GSDL_FLAG_SETTER): Likewise, from SCM_DEFINE_FLAG_SETTER. (SMOBGET, SMOBSET, SMOBFIELD): New macros. (GSTX_COLOR, GSTX_RECT, GSTX_SURFACE, GSTX_PIXEL_FORMAT, GSTX_RESERVED4, GSTX_RESERVED5, GSTX_RESERVED6, GSTX_RESERVED7, GSTX_TOO_MUCH): New numeric (index) macros. (gsdl_smob_tags): New var decl. (color_tag, rect_tag, surface_tag, pixel_format_tag): New tag abstaction macros. (ASSERT_COLOR, ASSERT_RECT, ASSERT_SURFACE, ASSERT_PIXEL_FORMAT): New ASSERT_SMOB abstraction macros.

--- include/sdlsmobs.h
+++ include/sdlsmobs.h
@@ -1,109 +1,75 @@
-/*******************************************************************
- *  sdlsmobs.h -- Smob helper definitions                          *
- *                                                                 *
- *  Created:    <2001-06-03 20:07:15 foof>                         *
- *  Time-stamp: <2001-07-07 21:50:26 foof>                         *
- *  Author:     Alex Shinn <foof@debian.org>                       *
- *                                                                 *
- *  Copyright (C) 2001 Alex Shinn                                  *
- *                                                                 *
- *  This program is free software; you can redistribute it and/or  *
- * modify it under the terms of the GNU General Public License as  *
- * published by the Free Software Foundation; either version 2 of  *
- * the License, or (at your option) any later version.             *
- *                                                                 *
- * This program is distributed in the hope that it will be useful, *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of  *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   *
- * GNU General Public License for more details.                    *
- *                                                                 *
- * You should have received a copy of the GNU General Public       *
- * License along with this program; if not, write to the Free      *
- * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,  *
- * MA 02111-1307 USA                                               *
- ******************************************************************/
+/* sdlsmobs.h --- Smob helper definitions
+ *
+ * 	Copyright (C) 2003 Thien-Thi Nguyen
+ * 	Copyright (C) 2001 Alex Shinn
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
 
-#ifndef _GUILE_SDL_SMOBS_H
-#define _GUILE_SDL_SMOBS_H
-
-#include <libguile.h>
-
-/* lots of SDL functions return 0 for true, -1 otherwise */
-#define SCM_RETURN_TRUE_IF_0(exp)  \
-  return ((exp) == 0) ? SCM_BOOL_T : SCM_BOOL_F
+#ifndef GUILE_SDL_SMOBS_H
+#define GUILE_SDL_SMOBS_H
 
 /* useful type-checking for smobs */
-#define SCM_ASSERT_SMOB(arg, tag, error, function)     \
-   SCM_ASSERT ((SCM_NIMP (arg)                         \
-                 && (long) SCM_CAR (arg) == tag),      \
-                arg, error, function)
+#define ASSERT_SMOB(arg, tag, error)                    \
+  SCM_ASSERT ((SCM_NIMP (arg)                           \
+                 && (long) SCM_CAR (arg) == tag),       \
+                arg, error, FUNC_NAME)
 
-/**********************************************************************/
+#define SMOBGET(smob,c_type)       ((c_type) SCM_SMOB_DATA (smob))
+#define SMOBSET(smob,val)          (SCM_SET_SMOB_DATA (smob, val))
 
-/* define a simple numeric getter */
-#define SCM_DEFINE_NUMBER_GETTER(s_func, c_func, c_tag, c_type, c_field) \
-SCM c_func (SCM s_smob)                                                  \
-{                                                                        \
-  SCM_ASSERT_SMOB (s_smob, c_tag, SCM_ARG1, s_func);                     \
-  return scm_long2num (((c_type) SCM_CDR (s_smob))->c_field);            \
-}
+#define SMOBFIELD(c_type,c_field)  (SMOBGET (smob, c_type)->c_field)
 
-/* define a simple numeric setter */
-#define SCM_DEFINE_NUMBER_SETTER(s_func, c_func, c_tag, c_type, c_field)    \
-SCM c_func (SCM s_smob, SCM s_value)                                        \
-{                                                                           \
-  SCM_ASSERT_SMOB (s_smob, c_tag, SCM_ARG1, s_func);                        \
-  SCM_ASSERT (scm_exact_p (s_value), s_value, SCM_ARG2, s_func);            \
-  (((c_type) SCM_CDR (s_smob))->c_field) = scm_num2long (s_value, SCM_ARG1, \
-                                                         "scm_num2long");   \
-  return SCM_UNSPECIFIED;                                                   \
-}
+
+/* Most smob tags are static, but a few are required to be global
+   because they are used in more than one compilation unit.  To minimize
+   linker symbol footprint we gather them into one big array, manually
+   indexed.  The convenience foo_tag macros are for consistency w/ the
+   static variants.  "GSTX" stands for "Guile-sdl (and/or Global) Smob
+   Tag indeX".  */
 
-/**********************************************************************/
+#define GSTX_COLOR              0
+#define GSTX_RECT               1
+#define GSTX_SURFACE            2
+#define GSTX_PIXEL_FORMAT       3
+#define GSTX_RESERVED4          4
+#define GSTX_RESERVED5          5
+#define GSTX_RESERVED6          6
+#define GSTX_RESERVED7          7
+#define GSTX_TOO_MUCH           8
 
-/* define a simple enum getter */
-#define SCM_DEFINE_ENUM_GETTER(s_func, c_func, c_tag, c_type, c_field, s_enum_type) \
-SCM c_func (SCM s_smob)                                                      \
-{                                                                            \
-  SCM_ASSERT_SMOB (s_smob, c_tag, SCM_ARG1, s_func);                         \
-  return scm_long2enum (((c_type) SCM_CDR (s_smob))->c_field, s_enum_type);  \
-}
+extern long gsdl_smob_tags[GSTX_TOO_MUCH];
 
-/* define a simple enum setter */
-#define SCM_DEFINE_ENUM_SETTER(s_func, c_func, c_tag, c_type, c_field, s_enum_type) \
-SCM c_func (SCM s_smob, SCM s_value)                                         \
-{                                                                            \
-  SCM_ASSERT_SMOB (s_smob, c_tag, SCM_ARG1, s_func);                         \
-  (((c_type) SCM_CDR (s_smob))->c_field) = scm_enum2long (s_value,           \
-                                                          s_enum_type,       \
-                                                          SCM_ARG1,          \
-                                                          "scm_enum2long");  \
-  return SCM_UNSPECIFIED;                                                    \
-}
+#define color_tag           (gsdl_smob_tags[GSTX_COLOR])
+#define rect_tag            (gsdl_smob_tags[GSTX_RECT])
+#define surface_tag         (gsdl_smob_tags[GSTX_SURFACE])
+#define pixel_format_tag    (gsdl_smob_tags[GSTX_PIXEL_FORMAT])
 
-/**********************************************************************/
+#define ASSERT_COLOR(obj,n)         ASSERT_SMOB (obj, color_tag, n)
+#define ASSERT_RECT(obj,n)          ASSERT_SMOB (obj, rect_tag, n)
+#define ASSERT_SURFACE(obj,n)       ASSERT_SMOB (obj, surface_tag, n)
+#define ASSERT_PIXEL_FORMAT(obj,n)  ASSERT_SMOB (obj, pixel_format_tag, n)
 
-/* define a simple flag getter */
-#define SCM_DEFINE_FLAG_GETTER(s_func, c_func, c_tag, c_type, c_field, s_flag_type) \
-SCM c_func (SCM s_smob)                                                      \
-{                                                                            \
-  SCM_ASSERT_SMOB (s_smob, c_tag, SCM_ARG1, s_func);                         \
-  return scm_ulong2flags (((c_type) SCM_CDR (s_smob))->c_field, s_flag_type);\
-}
+
+/* misc (this really belongs elsewhere) */
 
-/* define a simple flag setter */
-#define SCM_DEFINE_FLAG_SETTER(s_func, c_func, c_tag, c_type, c_field, s_flag_type) \
-SCM c_func (SCM s_smob, SCM s_value)                                         \
-{                                                                            \
-  SCM_ASSERT_SMOB (s_smob, c_tag, SCM_ARG1, s_func);                         \
-  (((c_type) SCM_CDR (s_smob))->c_field) = scm_flags2ulong (s_value,         \
-                                                            s_flag_type,     \
-                                                            SCM_ARG1,        \
-                                                            "scm_flag2long");\
-  return SCM_UNSPECIFIED;                                                    \
-}
+/* lots of SDL functions return 0 for true, -1 otherwise */
+#define RETURN_TRUE_IF_0(exp) \
+  return ((exp) == 0) ? SCM_BOOL_T : SCM_BOOL_F
 
-/**********************************************************************/
+#endif /* ! defined (GUILE_SDL_SMOBS_H) */
 
-#endif /* ! _GUILE_SDL_SMOBS_H */
-
+/* sdlsmobs.h ends here */
