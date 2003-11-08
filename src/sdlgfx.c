75858bed8b66014ad8ce62db1ad33d637c05809d
Thien-Thi Nguyen <ttn@gnuvola.org>
2003-11-08 15:42:42 +0000

    Update copyright. Include <guile/gh.h> and <SDL/SDL_image.h>. No longer include "sdlgfx.h". Include "config.h", "argcheck.h" and "sdlsmobs.h". Throughout, replace `SCM_DEFINE' with `MDEFLOCEXP'. Throughout, use gh_* funcs and other abstractions where possible. Write docstrings. (gsdl_gfx_init): Rename from `sdl_gfx_init'. Remove calls to `scm_c_define_gsubr'. Remove call to `scm_c_export'.

--- src/sdlgfx.c
+++ src/sdlgfx.c
@@ -1,126 +1,131 @@
-/*******************************************************************
- *  gfx.c -- Additional Graphics functions for Guile SDL           *
- *                                                                 *
- *  Created:    <2001-06-03 02:00:32 foof>                         *
- *  Time-stamp: <2001-06-25 00:23:48 foof>                         *
- *  Author:     Alex Shinn <foof@debian.org>                       *
- *                                                                 *
- *  Copyright (C) 2001 Alex Shinn                                  *
- *                                                                 *
- *  This program is free software; you can redistribute it and/or  *
- * modify it under the terms of the GNU General Public License as  *
- * published by the Free Software Foundation; either version 2 of  *
- * the License, or (at your option) any later version.             *
- *                                                                 *
- * This program is distributed in the hope that it will be useful, *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of  *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   *
- * GNU General Public License for more details.                    *
- *                                                                 *
- * You should have received a copy of the GNU General Public       *
- * License along with this program; if not, write to the Free      *
- * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,  *
- * MA 02111-1307 USA                                               *
- ******************************************************************/
+/* gfx.c --- Additional Graphics functions for Guile SDL
+ *
+ * 	Copyright (C) 2003 Thien-Thi Nguyen
+ * 	Copyright (C) 2001 Alex Shinn
+ *
+ *  This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
 
-#include "sdlgfx.h"
+#include <guile/gh.h>
+#include <SDL/SDL_image.h>
 
-SCM
-vertical_flip_surface (SCM s_surface)
+#include "config.h"
+#include "argcheck.h"
+#include "sdlsmobs.h"
+
+
+MDEFLOCEXP (vertical_flip_surface, "sdl-vertical-flip-surface", 1, 0, 0,
+            (SCM s_surface),
+            "Return a new surface created by flipping @var{surface} vertically.")
+#define FUNC_NAME s_vertical_flip_surface
 {
-   int i, w, h;
-   SDL_Surface *src, *dst;
-   SDL_Rect srcrect, dstrect;
+  int i, w, h;
+  SDL_Surface *src, *dst;
+  SDL_Rect srcrect, dstrect;
 
-   /* verify args */
-   SCM_ASSERT ((SCM_NIMP (s_surface)
-                && (long) SCM_CAR (s_surface) == surface_tag),
-               s_surface, SCM_ARG1, "sdl-vertical-flip-surface");
+  /* verify args */
+  ASSERT_SURFACE (s_surface, SCM_ARG1);
 
-   /* get source and dimensions */
-   src = (SDL_Surface *) SCM_CDR (s_surface);
-   w = src->w;
-   h = src->h;
+  /* get source and dimensions */
+  src = SMOBGET (s_surface, SDL_Surface *);
+  w = src->w;
+  h = src->h;
 
-   /* create a new surface */
-   dst = SDL_CreateRGBSurface (src->flags, w, h, 16, 0, 0, 0, 0);
+  /* create a new surface */
+  dst = SDL_CreateRGBSurface (src->flags, w, h, 16, 0, 0, 0, 0);
 
-   /* initialize the rects */
-   srcrect.x = 0;  srcrect.y = 0;    srcrect.w = w;  srcrect.h = 1;
-   dstrect.x = 0;  dstrect.y = h-1;  dstrect.w = w;  dstrect.h = 1;
+  /* initialize the rects */
+  srcrect.x = 0;  srcrect.y = 0;    srcrect.w = w;  srcrect.h = 1;
+  dstrect.x = 0;  dstrect.y = h-1;  dstrect.w = w;  dstrect.h = 1;
 
-   /* loop through, copying lines from top to bottom */
-   for (i=h; i>=0; i--) {
-      SDL_BlitSurface (src, &srcrect, dst, &dstrect);
-      srcrect.y++;
-      dstrect.y--;
-   }
+  /* loop through, copying lines from top to bottom */
+  for (i=h; i>=0; i--) {
+    SDL_BlitSurface (src, &srcrect, dst, &dstrect);
+    srcrect.y++;
+    dstrect.y--;
+  }
 
-   /* return the surface */
-   SCM_RETURN_NEWSMOB (surface_tag, dst);
+  /* return the surface */
+  SCM_RETURN_NEWSMOB (surface_tag, dst);
 }
+#undef FUNC_NAME
 
-SCM
-horiztonal_flip_surface (SCM s_surface)
+MDEFLOCEXP (horizontal_flip_surface, "sdl-horiztonal-flip-surface", 1, 0, 0,
+            (SCM s_surface),
+            "Return a new surface created by flipping @var{surface} horizontally.")
+#define FUNC_NAME s_horizontal_flip_surface
 {
-   int i, w, h;
-   SDL_Surface *src, *dst;
-   SDL_Rect srcrect, dstrect;
+  int i, w, h;
+  SDL_Surface *src, *dst;
+  SDL_Rect srcrect, dstrect;
 
-   /* verify args */
-   SCM_ASSERT ((SCM_NIMP (s_surface)
-                && (long) SCM_CAR (s_surface) == surface_tag),
-               s_surface, SCM_ARG1, "sdl-horiztonal-flip-surface");
+  /* verify args */
+  ASSERT_SURFACE (s_surface, SCM_ARG1);
 
-   /* get source and dimensions */
-   src = (SDL_Surface *) SCM_CDR (s_surface);
-   w = src->w;
-   h = src->h;
+  /* get source and dimensions */
+  src = SMOBGET (s_surface, SDL_Surface *);
+  w = src->w;
+  h = src->h;
 
-   /* create a new surface */
-   dst = SDL_CreateRGBSurface (src->flags, w, h, 16, 0, 0, 0, 0);
+  /* create a new surface */
+  dst = SDL_CreateRGBSurface (src->flags, w, h, 16, 0, 0, 0, 0);
 
-   /* initialize the rects */
-   srcrect.x = 0;    srcrect.y = 0;  srcrect.w = 1;  srcrect.h = h;
-   dstrect.x = w-1;  dstrect.y = 0;  dstrect.w = 1;  dstrect.h = h;
+  /* initialize the rects */
+  srcrect.x = 0;    srcrect.y = 0;  srcrect.w = 1;  srcrect.h = h;
+  dstrect.x = w-1;  dstrect.y = 0;  dstrect.w = 1;  dstrect.h = h;
 
-   /* loop through, copying lines from left to right */
-   for (i=w; i>=0; i--) {
-      SDL_BlitSurface (src, &srcrect, dst, &dstrect);
-      srcrect.x++;
-      dstrect.x--;
-   }
+  /* loop through, copying lines from left to right */
+  for (i=w; i>=0; i--) {
+    SDL_BlitSurface (src, &srcrect, dst, &dstrect);
+    srcrect.x++;
+    dstrect.x--;
+  }
 
-   /* return the surface */
-   SCM_RETURN_NEWSMOB (surface_tag, dst);
+  /* return the surface */
+  SCM_RETURN_NEWSMOB (surface_tag, dst);
 }
+#undef FUNC_NAME
 
-SCM
-vh_flip_surface (SCM s_surface)
+MDEFLOCEXP (vh_flip_surface, "sdl-vh-flip-surface", 1, 0, 0,
+            (SCM s_surface),
+            "Return a new surface created by flipping @var{surface}\n"
+            "both vertically and horizontally.")
+#define FUNC_NAME s_vh_flip_surface
 {
-   SCM temp = vertical_flip_surface (s_surface);
-   return horiztonal_flip_surface (temp);
+  SCM temp = vertical_flip_surface (s_surface);
+  return horizontal_flip_surface (temp);
 }
+#undef FUNC_NAME
 
-SCM
-scale_surface (SCM surface, SCM width, SCM height)
+MDEFLOCEXP (scale_surface, "sdl-scale-surface", 3, 0, 0,
+            (SCM surface, SCM width, SCM height),
+            "Scale @var{surface} by @var{width} and @var{height}.")
+#define FUNC_NAME s_scale_surface
 {
-   return SCM_UNSPECIFIED;
+  scm_misc_error (FUNC_NAME, "not yet implemented (sorry)", SCM_EOL);
+  return SCM_UNSPECIFIED;
 }
+#undef FUNC_NAME
 
 
+
 void
-sdl_gfx_init (void)
+gsdl_gfx_init (void)
 {
-   scm_c_define_gsubr ("sdl-vertical-flip-surface",    1, 0, 0, vertical_flip_surface);
-   scm_c_define_gsubr ("sdl-horizontal-flip-surface",  1, 0, 0, horiztonal_flip_surface);
-   scm_c_define_gsubr ("sdl-vh-flip-surface",          1, 0, 0, vh_flip_surface);
-   /* scm_c_define_gsubr ("sdl-scale-surface",            3, 0, 0, scale_surface); */
-
-   scm_c_export ("sdl-vertical-flip-surface",
-                 "sdl-horizontal-flip-surface",
-                 "sdl-vh-flip-surface",
-                 /* "scale-surface", */
-                 NULL);
+#include "sdlgfx.x"
 }
 
+/* sdlgfx.c ends here */
