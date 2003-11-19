163e7494ba3af6c1d8a41738983da0b1d8eb4683
Thien-Thi Nguyen <ttn@gnuvola.org>
2003-11-19 19:14:48 +0000

    Rename GH_DEFPROC funcs to omit leading "sdl_". Update associated "#define FUNC_NAME".

--- sdl-gfx/sdlgfx.c
+++ sdl-gfx/sdlgfx.c
@@ -30,11 +30,11 @@
 GH_USE_MODULE (sdlsup, "(sdl sdl-sup)"); /* for various gsdl_* C funcs */
 
 
-GH_DEFPROC (sdl_draw_point, "sdl-draw-point", 4, 0, 0,
+GH_DEFPROC (draw_point, "sdl-draw-point", 4, 0, 0,
             (SCM s_surface, SCM s_x, SCM s_y, SCM s_color),
             "On @var{surface}, draw a point at location\n"
             "@var{x},@var{y} with color @var{color}.")
-#define FUNC_NAME s_sdl_draw_point
+#define FUNC_NAME s_draw_point
 {
   ASSERT_SURFACE (s_surface, ARGH1);
   ASSERT_EXACT (s_x, ARGH2);
@@ -49,13 +49,13 @@ GH_DEFPROC (sdl_draw_point, "sdl-draw-point", 4, 0, 0,
 #undef FUNC_NAME
 
 
-GH_DEFPROC (sdl_draw_line, "sdl-draw-line", 6, 0, 0,
+GH_DEFPROC (draw_line, "sdl-draw-line", 6, 0, 0,
             (SCM s_surface, SCM s_x1, SCM s_y1,
              SCM s_x2, SCM s_y2, SCM s_color),
             "On @var{surface}, draw a line segment from\n"
             "@var{x1},@var{y1} to @var{x2},@var{y2},\n"
             "with color @var{color}.")
-#define FUNC_NAME s_sdl_draw_line
+#define FUNC_NAME s_draw_line
 {
   ASSERT_SURFACE (s_surface, ARGH1);
   ASSERT_EXACT (s_x1, ARGH2);
@@ -73,13 +73,13 @@ GH_DEFPROC (sdl_draw_line, "sdl-draw-line", 6, 0, 0,
 #undef FUNC_NAME
 
 
-GH_DEFPROC (sdl_draw_aa_line, "sdl-draw-aa-line", 6, 0, 0,
+GH_DEFPROC (draw_aa_line, "sdl-draw-aa-line", 6, 0, 0,
             (SCM s_surface, SCM s_x1, SCM s_y1,
              SCM s_x2, SCM s_y2, SCM s_color),
             "On @var{surface}, draw an anti-aliased line segment from\n"
             "@var{x1},@var{y1} to @var{x2},@var{y2},\n"
             "with color @var{color}.")
-#define FUNC_NAME s_sdl_draw_aa_line
+#define FUNC_NAME s_draw_aa_line
 {
   ASSERT_SURFACE (s_surface, ARGH1);
   ASSERT_EXACT (s_x1, ARGH2);
@@ -97,14 +97,14 @@ GH_DEFPROC (sdl_draw_aa_line, "sdl-draw-aa-line", 6, 0, 0,
 #undef FUNC_NAME
 
 
-GH_DEFPROC (sdl_draw_rectangle, "sdl-draw-rectangle", 6, 1, 0,
+GH_DEFPROC (draw_rectangle, "sdl-draw-rectangle", 6, 1, 0,
             (SCM s_surface, SCM s_x1, SCM s_y1, SCM s_x2, SCM s_y2,
              SCM s_color, SCM s_fill),
             "On @var{surface}, draw rectangle with opposite points\n"
             "@var{x1},@var{y1} and @var{x2},@var{y2},\n"
             "with color @var{color}.\n"
             "Optional arg @var{fill} means to fill the rectangle as well.")
-#define FUNC_NAME s_sdl_draw_rectangle
+#define FUNC_NAME s_draw_rectangle
 {
   ASSERT_SURFACE (s_surface, ARGH1);
   ASSERT_EXACT (s_x1, ARGH2);
@@ -125,13 +125,13 @@ GH_DEFPROC (sdl_draw_rectangle, "sdl-draw-rectangle", 6, 1, 0,
 #undef FUNC_NAME
 
 
-GH_DEFPROC (sdl_draw_circle, "sdl-draw-circle", 5, 1, 0,
+GH_DEFPROC (draw_circle, "sdl-draw-circle", 5, 1, 0,
             (SCM s_surface, SCM s_x, SCM s_y, SCM s_r,
              SCM s_color, SCM s_fill),
             "On @var{surface}, draw circle with center @var{x},@var{y}\n"
             "and radius @var{r}, with color @var{color}.\n"
             "Optional arg @var{fill} means to fill the circle as well.")
-#define FUNC_NAME s_sdl_draw_circle
+#define FUNC_NAME s_draw_circle
 {
   ASSERT_SURFACE (s_surface, ARGH1);
   ASSERT_EXACT (s_x, ARGH2);
@@ -151,13 +151,13 @@ GH_DEFPROC (sdl_draw_circle, "sdl-draw-circle", 5, 1, 0,
 #undef FUNC_NAME
 
 
-GH_DEFPROC (sdl_draw_ellipse, "sdl-draw-ellipse", 6, 1, 0,
+GH_DEFPROC (draw_ellipse, "sdl-draw-ellipse", 6, 1, 0,
             (SCM s_surface, SCM s_x, SCM s_y, SCM s_rx, SCM s_ry,
              SCM s_color, SCM s_fill),
             "On @var{surface}, draw ellipse with center @var{x},@var{y}\n"
             "x-radius @var{rx}, y-radius @var{ry}, with color @var{color}.\n"
             "Optional arg @var{fill} means to fill the ellipse as well.")
-#define FUNC_NAME s_sdl_draw_ellipse
+#define FUNC_NAME s_draw_ellipse
 {
   ASSERT_SURFACE (s_surface, ARGH1);
   ASSERT_EXACT (s_x, ARGH2);
@@ -178,13 +178,13 @@ GH_DEFPROC (sdl_draw_ellipse, "sdl-draw-ellipse", 6, 1, 0,
 #undef FUNC_NAME
 
 
-GH_DEFPROC (sdl_draw_polygon, "sdl-draw-polygon", 4, 1, 0,
+GH_DEFPROC (draw_polygon, "sdl-draw-polygon", 4, 1, 0,
             (SCM s_surface, SCM s_vx, SCM s_vy, SCM s_color, SCM s_fill),
             "On @var{surface}, draw a polygon whose points are specified\n"
             "by corresponding pairs from the uniform vectors\n"
             "@var{vx} and @var{vy}, in color @var{color}.  Optional\n"
             "arg @var{fill} means to fill the polygon as well.")
-#define FUNC_NAME s_sdl_draw_polygon
+#define FUNC_NAME s_draw_polygon
 {
   int ret;
   Sint16 *vx, *vy;
@@ -211,11 +211,11 @@ GH_DEFPROC (sdl_draw_polygon, "sdl-draw-polygon", 4, 1, 0,
 #undef FUNC_NAME
 
 
-GH_DEFPROC (sdl_draw_character, "sdl-draw-character", 5, 0, 0,
+GH_DEFPROC (draw_character, "sdl-draw-character", 5, 0, 0,
             (SCM s_surface, SCM s_x, SCM s_y, SCM s_char, SCM s_color),
             "On @var{surface} at position @var{x},@var{y},\n"
             "draw char @var{c} with @var{color} (a number).")
-#define FUNC_NAME s_sdl_draw_character
+#define FUNC_NAME s_draw_character
 {
   ASSERT_SURFACE (s_surface, ARGH1);
   ASSERT_EXACT (s_x, ARGH2);
@@ -231,11 +231,11 @@ GH_DEFPROC (sdl_draw_character, "sdl-draw-character", 5, 0, 0,
 #undef FUNC_NAME
 
 
-GH_DEFPROC (sdl_draw_string, "sdl-draw-string", 5, 0, 0,
+GH_DEFPROC (draw_string, "sdl-draw-string", 5, 0, 0,
             (SCM s_surface, SCM s_x, SCM s_y, SCM s_string, SCM s_color),
             "On @var{surface} at position @var{x},@var{y},\n"
             "draw string @var{text} with @var{color} (a number).")
-#define FUNC_NAME s_sdl_draw_string
+#define FUNC_NAME s_draw_string
 {
   ASSERT_SURFACE (s_surface, ARGH1);
   ASSERT_EXACT (s_x, ARGH2);
