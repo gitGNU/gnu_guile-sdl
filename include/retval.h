b3c9272b2b0e16ec9cacfe3b8b9976054cb4379f
Thien-Thi Nguyen <ttn@gnuvola.org>
2004-02-25 17:19:21 +0000

    Include "bool.h". Use `BOOL_FALSE' and `BOOL_TRUE'. Surround w/ #ifndef.

--- include/retval.h
+++ include/retval.h
@@ -1,8 +1,13 @@
 /* retval.h --- abstractions for `return FOO' */
 
+#ifndef GUILE_SDL_RETVAL_H
+#define GUILE_SDL_RETVAL_H
+
+#include "bool.h"
+
 /* Lots of SDL functions return 0 for true, -1 otherwise.  */
 #define RETURN_TRUE_IF_0(exp) \
-  return ((exp) == 0) ? SCM_BOOL_T : SCM_BOOL_F
+  return ((exp) == 0) ? BOOL_TRUE : BOOL_FALSE
 
 /* Some SDL functions have uninteresting return values.  */
 #define RETURN_UNSPECIFIED \
@@ -23,5 +28,7 @@
 /* Return a converted 0-terminated string.  */
 #define RETURN_0STR(exp) \
   return gh_str02scm (exp)
+
+#endif /* ! defined (GUILE_SDL_RETVAL_H) */
 
 /* retval.h ends here */
