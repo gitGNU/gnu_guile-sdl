44c89d6a73e404f5cdc43ee66e26b7ee9dd55e55
Thien-Thi Nguyen <ttn@gnuvola.org>
2004-02-25 18:39:07 +0000

    (RETURN_LIST0, _rv_PUSH, RETURN_LIST2, RETURN_LIST3, RETURN_LIST4, RETURN_LIST5, RETURN_LIST11): New macros.

--- include/retval.h
+++ include/retval.h
@@ -29,6 +29,70 @@
 #define RETURN_0STR(exp) \
   return gh_str02scm (exp)
 
+
+/* Return various lists of various length.  */
+
+#define RETURN_LIST0 \
+  return SCM_EOL
+
+#define _rv_PUSH(x) \
+  _rv = gh_cons ((x), _rv)
+
+#define RETURN_LIST2(a,b)                       \
+  do {                                          \
+    SCM _rv = SCM_EOL;                          \
+    _rv_PUSH (b);                               \
+    _rv_PUSH (a);                               \
+    return _rv;                                 \
+  } while (0)
+
+#define RETURN_LIST3(a,b,c)                     \
+  do {                                          \
+    SCM _rv = SCM_EOL;                          \
+    _rv_PUSH (c);                               \
+    _rv_PUSH (b);                               \
+    _rv_PUSH (a);                               \
+    return _rv;                                 \
+  } while (0)
+
+#define RETURN_LIST4(a,b,c,d)                   \
+  do {                                          \
+    SCM _rv = SCM_EOL;                          \
+    _rv_PUSH (d);                               \
+    _rv_PUSH (c);                               \
+    _rv_PUSH (b);                               \
+    _rv_PUSH (a);                               \
+    return _rv;                                 \
+  } while (0)
+
+#define RETURN_LIST5(a,b,c,d,e)                 \
+  do {                                          \
+    SCM _rv = SCM_EOL;                          \
+    _rv_PUSH (e);                               \
+    _rv_PUSH (d);                               \
+    _rv_PUSH (c);                               \
+    _rv_PUSH (b);                               \
+    _rv_PUSH (a);                               \
+    return _rv;                                 \
+  } while (0)
+
+#define RETURN_LIST11(a,b,c,d,e,f,g,h,i,j,k)    \
+  do {                                          \
+    SCM _rv = SCM_EOL;                          \
+    _rv_PUSH (k);                               \
+    _rv_PUSH (j);                               \
+    _rv_PUSH (i);                               \
+    _rv_PUSH (h);                               \
+    _rv_PUSH (g);                               \
+    _rv_PUSH (f);                               \
+    _rv_PUSH (e);                               \
+    _rv_PUSH (d);                               \
+    _rv_PUSH (c);                               \
+    _rv_PUSH (b);                               \
+    _rv_PUSH (a);                               \
+    return _rv;                                 \
+  } while (0)
+
 #endif /* ! defined (GUILE_SDL_RETVAL_H) */
 
 /* retval.h ends here */
