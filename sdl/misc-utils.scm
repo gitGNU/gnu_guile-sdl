912b53c120c881091e03f52bf9f33830988ad45c
Thien-Thi Nguyen <ttn@gnuvola.org>
2004-06-28 10:51:49 +0000

    Initial revision

---
+++ sdl/misc-utils.scm
@@ -0,0 +1,58 @@
+;;; misc-utils.scm --- grab bag of utility procs
+
+;;	Copyright (C) 2004 Thien-Thi Nguyen
+;;
+;; This is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This software is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this package; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+;; MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(define-module (sdl misc-utils)
+  #:use-module ((sdl sdl) #:renamer (symbol-prefix-proc 'SDL:))
+  #:export (call-with-clip-rect
+            rotate-square))
+
+;; Set default clip rect to @var{rect}, call @var{thunk}, and restore it.
+;; @var{thunk} is a procedure that takes no arguments.
+;;
+(define (call-with-clip-rect rect thunk)
+  (let* ((s (SDL:get-video-surface))
+         (orig (SDL:get-clip-rect s)))
+    (SDL:set-clip-rect! s rect)
+    (thunk)
+    (SDL:set-clip-rect! s orig)))
+
+;; Return a new surface made by rotating @var{square} by @var{angle} degrees.
+;; The square retains its original size.
+;;
+;;-sig: (square angle)
+;;
+(define (rotate-square src angle)
+  (let* ((width (SDL:surface:w src))
+         (height (SDL:surface:h src))
+         (rotated (SDL:roto-zoom-surface src (- angle 90) 1.0 #t))
+         (new-width (SDL:surface:w rotated))
+         (new-height (SDL:surface:h rotated))
+         (width-offset (quotient (- new-width width) 2))
+         (height-offset (quotient (- new-height height) 2))
+         (src-rect (SDL:make-rect width-offset height-offset width height))
+         (dst (SDL:make-surface width height))
+         (dst-rect (SDL:make-rect 0 0 width height)))
+    (SDL:blit-surface rotated src-rect dst dst-rect)
+    dst))
+
+;;; misc-utils.scm ends here
