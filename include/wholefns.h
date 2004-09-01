810414ead8d56d616d7529ff085b1d12155f7a85
Thien-Thi Nguyen <ttn@gnuvola.org>
2004-09-01 18:52:10 +0000

    Throughout, use "obj" instead of "smob".

--- include/wholefns.h
+++ include/wholefns.h
@@ -11,21 +11,21 @@
 /* number getter and setter */
 
 #define GSDL_NUMBER_GETTER(s_func, c_func, tag, c_type, c_field)        \
-GH_DEFPROC (c_func, s_func, 1, 0, 0, (SCM smob),                        \
+GH_DEFPROC (c_func, s_func, 1, 0, 0, (SCM obj),                         \
             "Get @code{" #c_field "} from @code{" #c_type "} object.")  \
 {                                                                       \
   const char *FUNC_NAME = s_ ## c_func;                                 \
-  ASSERT_SMOB (smob, tag, ARGH1);                                       \
+  ASSERT_SMOB (obj, tag, ARGH1);                                        \
   RETURN_INT (SMOBFIELD (c_type, c_field));                             \
 }
 
 #define GSDL_NUMBER_SETTER(s_func, c_func, tag, c_type, c_field)        \
-GH_DEFPROC (c_func, s_func, 2, 0, 0, (SCM smob, SCM value),             \
+GH_DEFPROC (c_func, s_func, 2, 0, 0, (SCM obj, SCM value),              \
             "Set @code{" #c_field "} in @code{" #c_type "} object\n"    \
             "to @var{value}.")                                          \
 {                                                                       \
   const char *FUNC_NAME = s_ ## c_func;                                 \
-  ASSERT_SMOB (smob, tag, ARGH1);                                       \
+  ASSERT_SMOB (obj, tag, ARGH1);                                        \
   ASSERT_EXACT (value, ARGH2);                                          \
   SMOBFIELD (c_type, c_field) = gh_scm2long (value);                    \
   RETURN_UNSPECIFIED;                                                   \
@@ -35,20 +35,20 @@ GH_DEFPROC (c_func, s_func, 2, 0, 0, (SCM smob, SCM value),             \
 /* enum getter and setter */
 
 #define GSDL_ENUM_GETTER(s_func, c_func, tag, c_type, c_field, etype)   \
-GH_DEFPROC (c_func, s_func, 1, 0, 0, (SCM smob),                        \
+GH_DEFPROC (c_func, s_func, 1, 0, 0, (SCM obj),                         \
             "")                                                         \
 {                                                                       \
   const char *FUNC_NAME = s_ ## c_func;                                 \
-  ASSERT_SMOB (smob, tag, ARGH1);                                       \
+  ASSERT_SMOB (obj, tag, ARGH1);                                        \
   return gsdl_long2enum (SMOBFIELD (c_type, c_field), etype);           \
 }
 
 #define GSDL_ENUM_SETTER(s_func, c_func, tag, c_type, c_field, etype)   \
-GH_DEFPROC (c_func, s_func, 2, 0, 0, (SCM smob, SCM value),             \
+GH_DEFPROC (c_func, s_func, 2, 0, 0, (SCM obj, SCM value),              \
             "")                                                         \
 {                                                                       \
   const char *FUNC_NAME = s_ ## c_func;                                 \
-  ASSERT_SMOB (smob, tag, ARGH1);                                       \
+  ASSERT_SMOB (obj, tag, ARGH1);                                        \
   SMOBFIELD (c_type, c_field)                                           \
     = gsdl_enum2long (value, etype, ARGH1, FUNC_NAME);                  \
   RETURN_UNSPECIFIED;                                                   \
@@ -58,20 +58,20 @@ GH_DEFPROC (c_func, s_func, 2, 0, 0, (SCM smob, SCM value),             \
 /* flag getter and setter*/
 
 #define GSDL_FLAG_GETTER(s_func, c_func, c_tag, c_type, c_field, stash) \
-GH_DEFPROC (c_func, s_func, 1, 0, 0, (SCM smob),                        \
+GH_DEFPROC (c_func, s_func, 1, 0, 0, (SCM obj),                         \
             "")                                                         \
 {                                                                       \
   const char *FUNC_NAME = s_ ## c_func;                                 \
-  ASSERT_SMOB (smob, c_tag, ARGH1);                                     \
+  ASSERT_SMOB (obj, c_tag, ARGH1);                                      \
   return gsdl_ulong2flags (SMOBFIELD (c_type, c_field), stash);         \
 }
 
 #define GSDL_FLAG_SETTER(s_func, c_func, c_tag, c_type, c_field, stash) \
-GH_DEFPROC (c_func, s_func, 2, 0, 0, (SCM smob, SCM value),             \
+GH_DEFPROC (c_func, s_func, 2, 0, 0, (SCM obj, SCM value),              \
             "")                                                         \
 {                                                                       \
   const char *FUNC_NAME = s_ ## c_func;                                 \
-  ASSERT_SMOB (smob, c_tag, ARGH1);                                     \
+  ASSERT_SMOB (obj, c_tag, ARGH1);                                      \
   SMOBFIELD (c_type, c_field)                                           \
     = GSDL_FLAGS2ULONG (value, stash, ARGH2);                           \
   RETURN_UNSPECIFIED;                                                   \
