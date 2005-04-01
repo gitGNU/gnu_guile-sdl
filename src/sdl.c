3abb7854d62e71fa447ad3a3c1ec9d805ea75d3c
Thien-Thi Nguyen <ttn@gnuvola.org>
2005-04-01 22:19:04 +0000

    (gsdl_init_misc): New extern func decl. (init_module): Call `gsdl_init_misc'.

--- src/sdl.c
+++ src/sdl.c
@@ -39,6 +39,7 @@ extern void gsdl_init_event (void);
 extern void gsdl_init_joystick (void);
 extern void gsdl_init_cdrom (void);
 extern void gsdl_init_gfx (void);
+extern void gsdl_init_misc (void);
 
 
 /* See ../include/sdlsmobs.h for discussion.
@@ -184,6 +185,7 @@ init_module (void)
   gsdl_init_joystick ();
   gsdl_init_cdrom ();
   gsdl_init_gfx ();
+  gsdl_init_misc ();
 }
 
 GH_MODULE_LINK_FUNC ("sdl sdl", sdl_sdl, init_module)
