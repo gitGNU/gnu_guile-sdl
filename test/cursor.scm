2abdbce04c96d7333e0e99a5f5d2c8e196346a0a
Thien-Thi Nguyen <ttn@gnuvola.org>
2006-07-21 21:59:05 +0000

    Use gnu-goatee.jpg for background. Loop a few times to make it more interesting.

--- test/cursor.scm
+++ test/cursor.scm
@@ -13,8 +13,8 @@
     (cond ((eq? modes #f)
            (error "no supported video modes"))
           ((eq? modes #t)
-           ;; any mode, use arbitrary 800x600
-           (SDL:make-rect 0 0 800 600))
+           ;; any mode, use 2x2 of gnu-goatee.jpg (200x153)
+           (SDL:make-rect 0 0 400 306))
           (else
            ;; a list - choose the first mode
            (car modes)))))
@@ -22,7 +22,19 @@
 ;; set the video mode to the dimensions of our rect
 (SDL:set-video-mode (SDL:rect:w test-rect) (SDL:rect:h test-rect) 16
                     '(SDL_HWSURFACE))
-(SDL:fill-rect (SDL:get-video-surface) test-rect #xffffff)
+(let* ((datadir (or (and=> (getenv "srcdir")
+                           (lambda (d) (in-vicinity d "test")))
+                    "."))
+       (gnu-head (SDL:load-image (in-vicinity datadir "gnu-goatee.jpg")))
+       (rect (SDL:make-rect 0 0 200 153)))
+  (do ((x 0 (1+ x)))
+      ((= 2 x))
+    (do ((y 0 (1+ y)))
+        ((= 2 y))
+      (SDL:rect:set-x! rect (* 200 x))
+      (SDL:rect:set-y! rect (* 153 y))
+      (or (= 0 (logxor x y))
+          (SDL:blit-surface gnu-head #f (SDL:get-video-surface) rect)))))
 (SDL:flip)
 
 ;; move the mouse (rude, but we can live w/ that)
@@ -32,6 +44,15 @@
 (SDL:delay 1000)
 
 ;; primitive cursor creation (need a higher level one)
+(define (random-16)
+  (list->vector (map (lambda ignored
+                       (random 256))
+                     (iota 16))))
+(do ((i 0 (1+ i)))
+    ((= 50 i))
+  (SDL:set-cursor (SDL:create-cursor (random-16) (random-16) 8 16 0 0))
+  (SDL:delay 200))
+
 (let* ((data #(85 85 85 85 85 85 85 85 85 85 85 85 85 85 85 85))
        (mask data)
        (cursor (SDL:create-cursor data mask 8 16 0 0)))
