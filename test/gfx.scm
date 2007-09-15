ebe51639a73217675e967681407aef8b84af1fdb
Thien-Thi Nguyen <ttn@gnuvola.org>
2007-09-15 23:07:55 +0000

    (SCREEN): New var.  Throughout, use it instead of `(SDL:get-video-surface)'.

--- test/gfx.scm
+++ test/gfx.scm
@@ -14,8 +14,10 @@
 
 (set! *random-state* (seed->random-state (current-time)))
 
+(define SCREEN (SDL:get-video-surface))
+
 ;; clear the screen
-(SDL:fill-rect (SDL:get-video-surface) #f #xffffff)
+(SDL:fill-rect SCREEN #f #xffffff)
 (SDL:flip)
 
 ;; draw beziers
@@ -28,55 +30,53 @@
         ((= i n))
       (vector-set! x-uv i (random 640))
       (vector-set! y-uv i (random 480)))
-    (SDL:draw-bezier (SDL:get-video-surface) x-uv y-uv 5
+    (SDL:draw-bezier SCREEN x-uv y-uv 5
                      (+ #x80 (ash (random #xffffff) 8))))
   (SDL:flip))
 
 ;; draw horizontal and vertical lines
-(let* ((surface (SDL:get-video-surface))
-       (w (SDL:surface:w surface))
-       (h (SDL:surface:h surface)))
+(let ((w (SDL:surface:w SCREEN))
+      (h (SDL:surface:h SCREEN)))
   (do ((i 0 (1+ i)))
       ((= i h))
-    (SDL:draw-hline surface (random w) (random w) i (random #xffffff)))
+    (SDL:draw-hline SCREEN (random w) (random w) i (random #xffffff)))
   (do ((i 0 (1+ i)))
       ((= i w))
-    (SDL:draw-vline surface i (random h) (random h) (random #xffffff)))
+    (SDL:draw-vline SCREEN i (random h) (random h) (random #xffffff)))
   (SDL:flip))
 
 ;; draw lines
 (define (one start-l start-r start-t start-b)
-  (let ((surface (SDL:get-video-surface)))
 
-    (define (line!-proc color)
-      (lambda (x1 y1 x2 y2)
-        (SDL:draw-line surface x1 y1 x2 y2 color)))
+  (define (line!-proc color)
+    (lambda (x1 y1 x2 y2)
+      (SDL:draw-line SCREEN x1 y1 x2 y2 color)))
 
-    (define (span-points beg end steps)
-      (let ((inc (/ (- end beg) steps)))
-        (let loop ((ls (list end)))
-          (if (<= (inexact->exact (car ls)) beg)
-              (map inexact->exact ls)
-              (loop (cons (- (car ls) inc) ls))))))
+  (define (span-points beg end steps)
+    (let ((inc (/ (- end beg) steps)))
+      (let loop ((ls (list end)))
+        (if (<= (inexact->exact (car ls)) beg)
+            (map inexact->exact ls)
+            (loop (cons (- (car ls) inc) ls))))))
 
-    (define (mesh l r t b steps)
-      (let* ((LR (span-points l r steps))
-             (RL (reverse LR))
-             (TB (span-points t b steps))
-             (BT (reverse TB))
-             (L  (make-list (1+ steps) l))
-             (R  (make-list (1+ steps) r))
-             (T  (make-list (1+ steps) t))
-             (B  (make-list (1+ steps) b))
-             (d! (line!-proc (random #xffffff))))
-        (map d! LR T R TB)
-        (map d! R TB RL B)
-        (map d! RL B L BT)
-        (map d! L BT LR T)))
+  (define (mesh l r t b steps)
+    (let* ((LR (span-points l r steps))
+           (RL (reverse LR))
+           (TB (span-points t b steps))
+           (BT (reverse TB))
+           (L  (make-list (1+ steps) l))
+           (R  (make-list (1+ steps) r))
+           (T  (make-list (1+ steps) t))
+           (B  (make-list (1+ steps) b))
+           (d! (line!-proc (random #xffffff))))
+      (map d! LR T R TB)
+      (map d! R TB RL B)
+      (map d! RL B L BT)
+      (map d! L BT LR T)))
 
-    (mesh start-l start-r start-t start-b 20)
-    (mesh (+ start-l 75) (- start-r 75) (+ start-t 75) (- start-b 75) 15)
-    (mesh (+ start-l 150) (- start-r 150) (+ start-t 150) (- start-b 150) 10)))
+  (mesh start-l start-r start-t start-b 20)
+  (mesh (+ start-l 75) (- start-r 75) (+ start-t 75) (- start-b 75) 15)
+  (mesh (+ start-l 150) (- start-r 150) (+ start-t 150) (- start-b 150) 10))
 
 (do ((i 0 (1+ i)))
     ((= i 30))
@@ -87,7 +87,7 @@
 (do ((x-radius 320 (1- x-radius))
      (y-radius 240 (1- y-radius)))
     ((= 200 y-radius))
-  (SDL:draw-ellipse (SDL:get-video-surface) 320 240 x-radius y-radius
+  (SDL:draw-ellipse SCREEN 320 240 x-radius y-radius
                     (+ (ash (random #xffffff) 8) 1)
                     #t)
   (SDL:flip))
@@ -96,7 +96,7 @@
 (do ((radius 100 (1- radius)))
     ((> 70 radius))
   (let ((c! (lambda (x y)
-              (SDL:draw-circle (SDL:get-video-surface) x y radius
+              (SDL:draw-circle SCREEN x y radius
                                (+ (ash (random #xffffff) 8) 1)
                                #t))))
     (c! 100 100)
@@ -106,9 +106,8 @@
   (SDL:flip))
 
 ;; draw pie slices
-(let* ((surface (SDL:get-video-surface))
-       (w (SDL:surface:w surface))
-       (h (SDL:surface:h surface)))
+(let ((w (SDL:surface:w SCREEN))
+      (h (SDL:surface:h SCREEN)))
   (do ((slice 0 (1+ slice)))
       ((= 42 slice))
     (let* ((x (random w))
@@ -118,7 +117,7 @@
            (sub (quotient (- (+ beg (random 360)) beg) 16)))
       (do ((i 0 (1+ i)))
           ((= i 16))
-        (SDL:draw-pie-slice surface
+        (SDL:draw-pie-slice SCREEN
                             x y (min x (- w x) y (- h y))
                             (+ beg (* sub i)) (+ beg (* sub (1+ i)))
                             (+ (* 9 i) color)
@@ -135,7 +134,7 @@
         ((= i n))
       (vector-set! x-uv i (random 640))
       (vector-set! y-uv i (random 480)))
-    (SDL:draw-polygon (SDL:get-video-surface) x-uv y-uv
+    (SDL:draw-polygon SCREEN x-uv y-uv
                       (+ #x80 (ash (random #xffffff) 8))
                       #t))
   (SDL:flip))
