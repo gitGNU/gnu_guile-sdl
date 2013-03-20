578bf7b3cd344f335115b4b18bf7ab4dcdca6779
Thien-Thi Nguyen <ttn@gnuvola.org>
2013-03-20 14:39:35 +0100

    Comment munging; nfc.

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 # Usage: sh -x ./autogen.sh [--libtoolize]
-#
-# Building Guile-SDL has been tested with the following toolset:
-#  GNU Libtool 2.4.2
-#  GNU Autoconf 2.69
-#  GNU Automake 1.12.4
-#  Unofficial Guile 1.4.1.127
-#  GNU Guile 1.8.7
-#  GNU Guile 2.0.6
-#  GNU texinfo 4.13
-#  Guile-BAUX 20121026.1208.c21074a
-#  SNUGGLE 0.1
 
 [ -f src/misc-utils.scm ] || {
   echo "autogen.sh: run this command only in the guile-sdl directory."
